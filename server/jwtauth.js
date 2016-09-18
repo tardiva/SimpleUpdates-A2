7e5b55857b611893337a480148c318615bc74873
Igor Romanov <igor@barabara.ru>
2016-09-18 18:19:12 +0300

    Updates from Friday session

--- server/jwtauth.js
+++ server/jwtauth.js
@@ -1,40 +1,40 @@
 var jwt = require('jwt-simple');
 var mysql = require('mysql');
 
-module.exports = function(req, res, next) { 
-        
-        var token = req.headers['x-access-token'];
-        
-        if (token) {
+module.exports = function (req, res, next) {
+
+    var token = req.headers['x-access-token'];
+
+    if (token) {
         try {
-        var decoded = jwt.decode(token, req.tokenKey);
-        
-        if (decoded.exp <= Date.now()) {
+            var decoded = jwt.decode(token, req.tokenKey);
+
+            if (decoded.exp <= Date.now()) {
                 res.status(401).send('Unauthorized');
-        }
+            }
             else {
                 var query = "SELECT `id` FROM ?? WHERE `id` = ?";
                 var table = ["users", decoded.iss];
-                query = mysql.format(query,table);
-                req.connection.query(query,function(err,rows){
-                    if(err) {
-                        res.json({"Error" : true, "Message" : "Error executing MySQL query"});}
-                    else if (!rows[0]){
-                        res.status(401).send('Unauthorized');}
+                query = mysql.format(query, table);
+                req.connection.query(query, function (err, rows) {
+                    if (err) {
+                        res.json({"Error": true, "Message": "Error executing MySQL query"});
+                    }
                     else if (rows[0]) {
                         req.user_id = decoded.iss;
-                                      next();}
+                        next();
+                    }
+
+                    res.status(401).send('Unauthorized');
                 })
             }
 
         } catch (err) {
-        res.status(401).send('Unauthorized');
+            res.status(401).send('Unauthorized');
         }
-        } else {
+    } else {
         res.status(401).send('Unauthorized');
-        }
-        
-    
-} 
+    }
+};
 
 
