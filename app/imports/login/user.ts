ca9ceee220d70865e79554d424b6437b139f435b
Irina <rannveig@mail.ru>
2016-09-20 20:59:47 +0300

    removed imports directory

--- app/imports/login/user.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-    
-    id?: number;
-    tenant_id?: number;
-    first_name?: string;
-    last_name?: string;
-    email?: string;
-    password?: any;
-}
--- app/imports/updates/update.ts
+++
@@ -1,9 +0,0 @@
-export interface Update {
-    
-    update_id?: number;
-    project_id: number;
-    update_date?: Date;
-    update_status: number;
-    update_text: string;
-    updated_by: number;
-}
